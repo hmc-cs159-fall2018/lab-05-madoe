724eb281738d8c43e36493feecd414d526faffad
Chloe Elliott <chloegantzel@gmail.com>
2018-10-04 22:09:46 +0000

    added stuff

--- SpellChecker.py
+++ SpellChecker.py
@@ -3,4 +3,19 @@ import LanguageModel
 
 class SpellChecker():
 
-    
+    def __init__(self, channel_model=None, language_model=None, max_distance):
+        ''' takes in EditDistanceFinder object as channel_model, 
+        LanguageModel object as language_model, and an int as max_distance
+        to initialize the SpellChecker. '''
+        self.nlp = spacy.load("en", pipeline=["tagger", "parser"])
+        self.channel_model = channel_model
+        self.language_model = language_model
+        self.max_distance = max_distance
+
+
+    def bigram_score(self, prev_word, focus_word, next_word):
+        ''' Take in 3 words and return average of bigram probability 
+        for both bigrams of the sequence. '''
+        score1 = self.language_model.bigram_prob(prev_word, focus_word)
+        score2 = self.language_model.bigram_prob(focus_word, next_word)
+        return (score1 + score2)/2
