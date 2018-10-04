99ec1c32bb67f3b4378bb9a10a03d3e920d68d9d
MadHobbs <mhobbs0760@scrippscollege.edu>
2018-10-04 15:10:26 -0700

    load channel and language

--- SpellChecker.py
+++ SpellChecker.py
@@ -1,6 +1,22 @@
 import EditDistance
 import LanguageModel
+import pickle
 
 class SpellChecker():
 
-    
+    def load_channel_model(fp):
+        ''' Takes in a file pointer as input
+        and should initialize the SpellChecker object’s 
+        channel_model data member to a default EditDistanceFinder 
+        and then load the stored language model (e.g. ed.pkl) 
+        from fp into that data member. '''
+        self.channel_model = EditDistance.EditDistanceFinder()
+        self.channel_model = pickle.load(fp)
+
+    def load_language_model(fp):
+        ''' Takes in a file pointer as input and should initialize the 
+        SpellChecker object’s language_model data member to a default 
+        LanguageModel and then load the stored language model (e.g. lm.pkl) 
+        from fp into that data member. ''' 
+        self.language_model = LanguageModel.LanguageModel()
+        self.language_model = pickle.load(fp)
