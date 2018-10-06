ad2773915210b68f3e398a0b0ae8675b53037432
MadHobbs <mhobbs0760@scrippscollege.edu>
2018-10-05 17:24:19 -0700

    working on insert

--- SpellChecker.py
+++ SpellChecker.py
@@ -22,7 +22,7 @@ class SpellChecker():
         and then load the stored language model (e.g. ed.pkl) 
         from fp into that data member. '''
         self.channel_model = EditDistance.EditDistanceFinder()
-        self.channel_model = self.channel_model.load(fp)
+        self.channel_model.load(fp)
 
     def load_language_model(self, fp):
         ''' Takes in a file pointer as input and should initialize the 
@@ -30,7 +30,7 @@ class SpellChecker():
         LanguageModel and then load the stored language model (e.g. lm.pkl) 
         from fp into that data member. ''' 
         self.language_model = LanguageModel.LanguageModel()
-        self.language_model = self.language_model.load(fp)
+        self.language_model.load(fp)
 
     def bigram_score(self, prev_word, focus_word, next_word):
         ''' Take in 3 words and return average of bigram probability 
@@ -52,15 +52,27 @@ class SpellChecker():
             # if we inserted 1 char to get from observed word to intended_word
             if len(word) + 1 == len(intended_word):
                 alignment = self.channel_model.align(word, intended_word)
-                print(alignment)
+                distance, tuples = self.channel_model.align("suprise", "surprise")
+                observed = ""
+                intended = ""
+                perc_count = 0
+                for t in tuples:
+                    if perc_count > 1: break
+                    if t[0] != t[1] or t[0] != "%": break
+                    if t[0]=="%":
+                        perc_count += 1
+                    observed += t[0]
+                    intended += t[1]
+                print(observed)
+                print(intended)
+
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ed", type=argparse.FileType('rb'), required=True)
-    parser.add_argument("--lm", type=argparse.FileType('rb'), required = True)
+    parser.add_argument("--ed", type=argparse.FileType('rb'))
+    parser.add_argument("--lm", type=argparse.FileType('rb'))
     args = parser.parse_args()
 
     sp = SpellChecker(max_distance = 2)
     sp.load_channel_model(args.ed)
-    sp.load_language_model(args.lm)
